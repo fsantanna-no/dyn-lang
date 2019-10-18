e47c4fe1ef887c5ec0151eaadffb212db71710bc
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-10-18 08:18:09 -0300

    (+) Eval.eval*

--- Dyn/Eval.hs
+++ Dyn/Eval.hs
@@ -6,3 +6,13 @@ import Dyn.AST
 
 evalExpr :: Expr -> Expr
 evalExpr (EUnit z) = EUnit z
+
+-------------------------------------------------------------------------------
+
+evalDcl :: Dcl -> Expr
+evalDcl (Dcl (_, id, _, w)) = evalWhere w
+
+-------------------------------------------------------------------------------
+
+evalWhere :: Where -> Expr
+evalWhere (Where (_, e, [])) = evalExpr e
--- EvalSpec.hs
+++ EvalSpec.hs
@@ -11,3 +11,21 @@ main = hspec $ do
   describe "evalExpr:" $ do
     it "()" $
       evalExpr (EUnit az) `shouldBe` (EUnit az)
+
+  describe "evalWhere:" $ do
+    it "()" $
+      evalWhere
+        (Where (az, EUnit az, []))
+        `shouldBe` (EUnit az)
+    it "b where b=a, a=()" $
+      evalWhere
+        (Where (az, EVar az "b", [
+          Dcl (az, "b", (), Where (az, EVar az "a",[])),
+          Dcl (az, "a", (), Where (az, EUnit az,[]))
+        ]))
+        `shouldBe` (EUnit az)
+
+  describe "evalDcl:" $ do
+    it "a=()" $
+      evalDcl (Dcl (az, "a", (), Where (az, EUnit az,[])))
+        `shouldBe` (EUnit az)
