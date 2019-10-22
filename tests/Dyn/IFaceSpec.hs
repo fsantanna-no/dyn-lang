e6e3b0d9c3b7a6c4614294185feb3415cf3ed397
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-10-22 20:09:09 -0300

    (>) tests.IFace: IEq default eq for tuples

--- tests/Dyn/IFaceSpec.hs
+++ tests/Dyn/IFaceSpec.hs
@@ -16,15 +16,24 @@ spec = do
 
   describe "manual" $ do
 
-    it "IEqualable: overrides eq" $
+    it "IEqualable: default eq" $
       run ([r|
 main = v where  -- neq (eq(T,T), F)
   v = neq_ ((eq_,neq_), eq_ ((eq_,neq_),Bool.True,Bool.True), Bool.False)
 ;
-|] ++ ieq ++ bool)
+|] ++ bool ++ ieq)
         `shouldBe` "Bool.True"
 
-    it "IEqualable: (eq(T,F) == (F,T)" $
+    it "IEqualable: (eq ((T,F),(F,T)), eq ((T,F),(T,F))" $
+      run ([r|
+main = (x,y) where
+  x = eq_ ((eq_,neq_), (Bool.True,Bool.False), (Bool.False,Bool.True))
+  y = eq_ ((eq_,neq_), (Bool.True,Bool.False), (Bool.True,Bool.False))
+;
+|] ++ bool ++ ieq)
+        `shouldBe` "(Bool.False,Bool.True)"
+
+    it "IEqualable: overrides eq (ieq_bool)" $
       run ([r|
 main = v where  -- neq (eq(T,T), F)
   -- typesystem renames to concrete type methods
@@ -45,30 +54,6 @@ main = v where  -- (T<=F, T>=T, F>F, F<T)
         `shouldBe` "(Bool.False,Bool.True,Bool.False,Bool.True)"
 
 -------------------------------------------------------------------------------
-
-bool = [r|
-  not = func ->
-    case ... of
-      Bool.False -> Bool.True
-      Bool.True  -> Bool.False
-    ;
-  ;
-
-  and = func ->
-    case ... of
-      (Bool.False, _) -> Bool.False
-      (_, Bool.False) -> Bool.False
-      _               -> Bool.True
-    ;
-  ;
-
-  or = func ->
-    case ... of
-      (Bool.True, _)  -> Bool.True
-      (_,         =y) -> y
-    ;
-  ;
-|]
 
 -- implementation IOrderable for Bool
 iord_bool = [r|
@@ -129,7 +114,32 @@ ieq_bool = [r|
   ;
 |]
 
--- interface IOrderable
+-- Bool type: not, and, or
+bool = [r|
+  not = func ->
+    case ... of
+      Bool.False -> Bool.True
+      Bool.True  -> Bool.False
+    ;
+  ;
+
+  and = func ->
+    case ... of
+      (Bool.False, _) -> Bool.False
+      (_, Bool.False) -> Bool.False
+      _               -> Bool.True
+    ;
+  ;
+
+  or = func ->
+    case ... of
+      (Bool.True, _)  -> Bool.True
+      (_,         =y) -> y
+    ;
+  ;
+|]
+
+-- interface IOrd(lt,lte,dt,gte)
 iord = [r|
   -- lt_ = ???
   lte_ = func ->
@@ -161,7 +171,7 @@ ieq = [r|
       (~y,~x) -> Bool.True
       _       -> Bool.False
     ; where
-      (x,y) = ...
+      (_,x,y) = ...
     ;
   ;
   neq_ = func ->
