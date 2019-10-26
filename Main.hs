4ca06a29856c5a9c8fe6d7753329e5831c22cd62
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-10-26 17:45:51 -0300

    (+) Main: execute full test file

---
+++ Main.hs
@@ -0,0 +1,14 @@
+import Test.HUnit
+import System.IO (hPutStrLn, stderr)
+import System.Directory
+import System.Process
+import System.Exit
+import Debug.Trace
+
+import Dyn.Eval
+
+main :: IO ()
+main = do
+    src <- readFile "/tmp/x.tch"
+    putStrLn $ run src
+    return ()
