67d14357566e02cb1fe0a09eec85e5c36b92d2b9
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-11-12 18:26:18 -0200

    (>) Map.map: simplify mapPatt (remove decls)

--- Dyn/Map.hs
+++ Dyn/Map.hs
@@ -14,6 +14,7 @@ type MapFs = ( ([Glob]->Ctrs->[Decl]->[Decl]->[Decl]),
                ([Glob]->Ctrs->[Decl]->Type->ExpWhere->ExpWhere),
                ([Glob]->Ctrs->[Decl]->Patt->Patt),
                ([Glob]->Ctrs->[Decl]->Type->Expr->Expr))
+
 mSz _ _ _   ds = ds
 mDz _ _ _   d  = [d]
 mWz _ _ _ _ w  = w
@@ -34,8 +35,8 @@ mapDecl :: MapFs -> [Glob] -> Ctrs -> [Decl] -> Decl -> [Decl]
 mapDecl fs@(_,fD,_,_,_) globs ctrs dsigs decl@(DSig _ _ _ _) = fD globs ctrs dsigs decl
 mapDecl fs@(_,fD,_,_,_) globs ctrs dsigs (DAtr z pat whe)    = fD globs ctrs dsigs $ DAtr z pat' whe'
   where
-    (_,pat') = mapPatt  fs globs ctrs dsigs TAny pat
-    whe'     = mapWhere fs globs ctrs dsigs (toType dsigs pat) whe
+    pat' = mapPatt  fs globs ctrs dsigs TAny pat
+    whe' = mapWhere fs globs ctrs dsigs (toType dsigs pat) whe
 
 -------------------------------------------------------------------------------
 
@@ -45,51 +46,45 @@ mapWhere fs@(_,_,fW,_,_) globs ctrs dsigs xtp (ExpWhere (z,ds,e)) = fW globs ctr
     e'  = mapExpr  fs globs ctrs dsigs'' xtp e
     ds' = mapDecls fs globs ctrs dsigs'  ds
 
-    dsigs''   = filter isDSig ds' ++ dsigs
-    dsigs'    = filter isDSig ds  ++ dsigs
+    dsigs'' = filter isDSig ds' ++ dsigs
+    dsigs'  = filter isDSig ds  ++ dsigs
 
 -------------------------------------------------------------------------------
 
-mapPatt :: MapFs -> [Glob] -> Ctrs -> [Decl] -> Type -> Patt -> ([Decl],Patt)
-mapPatt fs@(_,_,_,fP,_) globs ctrs dsigs xtp p = (ds', fP globs ctrs dsigs p') where
-  (ds',p') = aux p
+mapPatt :: MapFs -> [Glob] -> Ctrs -> [Decl] -> Type -> Patt -> Patt
+mapPatt fs@(_,_,_,fP,_) globs ctrs dsigs xtp p = fP globs ctrs dsigs (rec p) where
 
   -- TODO: TAny
-  aux (PWrite z id)    = (dsig, PWrite z id) where
-                          dsig = bool [DSig z id cz xtp] [] (xtp==TAny)
-  aux (PRead  z e)     = ([], PRead z $ mapExpr fs globs ctrs dsigs TAny e) -- TODO: xtp
-  aux (PTuple z ps)    = (concat ds', PTuple z ps') where
-                          (ds',ps') = unzip $ map (mapPatt fs globs ctrs dsigs TAny) ps
-  aux (PCall  z p1 p2) = (ds2'++ds1', PCall z p1' p2') where
-                          (ds1',p1') = mapPatt fs globs ctrs dsigs TAny p1
-                          (ds2',p2') = mapPatt fs globs ctrs dsigs TAny p2
-  aux p                = ([], p)
+  rec (PRead  z e)     = PRead z $ mapExpr fs globs ctrs dsigs TAny e -- TODO: xtp
+  rec (PTuple z ps)    = PTuple z $ map (mapPatt fs globs ctrs dsigs TAny) ps
+  rec (PCall  z p1 p2) = PCall z p1' p2' where
+                          p1' = mapPatt fs globs ctrs dsigs TAny p1
+                          p2' = mapPatt fs globs ctrs dsigs TAny p2
+  rec p                = p
 
 -------------------------------------------------------------------------------
 
 mapExpr :: MapFs -> [Glob] -> Ctrs -> [Decl] -> Type -> Expr -> Expr
-mapExpr fs@(_,_,_,_,fE) globs ctrs dsigs xtp e = fE globs ctrs dsigs xtp (aux e) where
-  aux (EFunc  z cs tp ups whe) = EFunc  z cs tp ups $ mapWhere fs globs ctrs' (arg:dsigs) xtp whe where
+mapExpr fs@(_,_,_,_,fE) globs ctrs dsigs xtp e = fE globs ctrs dsigs xtp (rec e) where
+  rec (EFunc  z cs tp ups whe) = EFunc  z cs tp ups $ mapWhere fs globs ctrs' (arg:dsigs) xtp whe where
                                   ctrs' = Ctrs $ getCtrs ctrs ++ getCtrs cs
                                   arg = DSig z "..." cz inp
                                   (inp,xtp) = case tp of
                                                 TFunc inp out -> (inp,out)
                                                 otherwise     -> (TAny,TAny)
   -- TODO: TAny
-  aux (EData  z hr e)          = EData  z hr $ mapExpr fs globs ctrs dsigs TAny e
-  aux (ETuple z es)            = ETuple z $ map (mapExpr fs globs ctrs dsigs TAny) es
-  aux (ECall  z e1 e2)         = ECall  z e1' e2' where
+  rec (EData  z hr e)          = EData  z hr $ mapExpr fs globs ctrs dsigs TAny e
+  rec (ETuple z es)            = ETuple z $ map (mapExpr fs globs ctrs dsigs TAny) es
+  rec (ECall  z e1 e2)         = ECall  z e1' e2' where
                                   e1' = mapExpr fs globs ctrs dsigs TAny e1
                                   e2' = mapExpr fs globs ctrs dsigs TAny e2
-  aux (ECase  z e l)           = ECase  z e' l' where
-    e'         = mapExpr fs globs ctrs dsigs TAny e
-    (ps, ws)   = unzip l
-    (dss',ps') = unzip $ map (mapPatt fs globs ctrs dsigs xtp) ps
-    ws'        = map f (zip dss' ws) where
-                  f (ds,w) = mapWhere fs globs ctrs dsigs' xtp w where
-                              dsigs' = (filter isDSig ds) ++ dsigs
-    l'         = zip ps' ws'
-  aux e                        = e
+  rec (ECase  z e l)           = ECase  z e' l' where
+    e' = mapExpr fs globs ctrs dsigs TAny e
+    l' = map f l where
+          f (pat,whe) = (pat',whe') where
+            pat' = mapPatt  fs globs ctrs dsigs xtp pat
+            whe' = mapWhere fs globs ctrs dsigs xtp whe
+  rec e      = e
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -111,7 +106,8 @@ collectDecl fs (DSig _ _ _ _)   = S.empty
 collectDecl fs (DAtr _ pat whe) = S.union (collectPatt fs pat) (collectWhere fs whe)
 
 collectWhere :: Ord a => CollectFs a -> ExpWhere -> S.Set a
-collectWhere fs@(CollectFs (fW,_,_)) w@(ExpWhere (_,ds,e)) = fW w $ S.union (collectDecls fs ds) (collectExpr fs e)
+collectWhere fs@(CollectFs (fW,_,_)) w@(ExpWhere (_,ds,e)) =
+  fW w $ S.union (collectDecls fs ds) (collectExpr fs e)
 
 collectPatt :: Ord a => CollectFs a -> Patt -> S.Set a
 collectPatt fs@(CollectFs (_,fP,_)) p = S.union (fP p) (rec p) where
