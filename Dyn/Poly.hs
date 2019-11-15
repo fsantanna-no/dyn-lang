9c98501ac1499989c2a09c8578b3579c4a2b1725
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-11-15 16:01:25 -0200

    (>) Poly: recursive TData

--- Dyn/Poly.hs
+++ Dyn/Poly.hs
@@ -1,6 +1,8 @@
 module Dyn.Poly where
 
 import Debug.Trace
+import Data.Bool (bool)
+import qualified Data.List as L
 import qualified Data.Set  as S
 import qualified Data.Map  as M
 
@@ -27,19 +29,23 @@ mE globs cts dsigs xtp e@(EVar z id) = e' where
   (dcs,dtp) = dsigsFind dsigs id
   dcs'      = Ifce.ifcesSups globs (getCtrs dcs) where
 
-  e' = case (xtp, dcs',dtp) of
+  e' = case (cts, xtp, dcs',dtp) of
 
-    -- local poly var
-    --    v where v::a
-    (_, [], TVar "a")     -> e
+    -- local poly var with enclosing recursive declaration
+    --    f :: (List of a -> ...) where a is IEnum
+    --      v where v::a
+    (l, _, [], TVar "a")
+      | isVarInRec "a" l     -> e --traceShow (id) $ toV "a" e
+    (l, _, [], TVar "a")     -> e
 
     -- id is not poly, nothing to do, just keep it
     --    Bool.True
-    (_, [], _)            -> e
+    (_, _, [], _)            -> e
 
     -- xtp is concrete, instantiate e with it
     --    maximum :: Bool
-    (TData xhr2 _, _, _)  -> f (concat xhr2)     -- TODO: TData ofs
+    --    maximum' dIBoundedBool
+    (_, TData xhr2 _, _, _)  -> toID' (concat xhr2)     -- TODO: TData ofs
 
     -- xtp is not concrete yet
     -- .
@@ -48,13 +54,36 @@ mE globs cts dsigs xtp e@(EVar z id) = e' where
     otherwise             -> e
 
     where
-      f suf = ECall z (EVar z $ id++"'")
-                      (fromList $ map g $ dcs') where
-        g ifc = if elem suf (getRecDatas globs dtp) then
-                  ECall z (EVar z "snd")
-                          (ECall z (EVar z $ "d"++ifc++suf) (EVar z id))
-                else
-                  EVar z $ "d" ++ ifc ++ suf
+      toID' suf = ECall z (EVar z $ id++"'")
+                        (fromList $ map (EVar z) $ map toID $ dcs') where
+                        toID ifc = "d" ++ ifc ++ suf -- dIEqa / dIEqBool
+
+      toV "a" e = ECall z (EVar z "snd")
+                          (ECall z (EVar z $ "d"++"TODO"++"a") e)
+
+      isVarInRec :: ID_Var -> CTs -> Bool
+      isVarInRec "a" l =
+        case L.find (has "a") $ map snd l of
+          Nothing -> False
+          Just tp -> any isRec $ datasWithVar "a" tp
+        where
+          isRec tp = elem "a" (getRecDatas globs tp)
+
+      datasWithVar :: ID_Var -> Type -> [Type]
+      datasWithVar  _  TAny             = []
+      datasWithVar  _  TUnit            = []
+      datasWithVar  _  (TVar _)         = []
+      datasWithVar "a" tp@(TData _ ofs) = bool [] [tp] (any (has "a") ofs)
+      datasWithVar "a" (TTuple tps)     = concatMap (datasWithVar "a") tps
+      datasWithVar "a" (TFunc inp out)  = datasWithVar "a" inp ++ datasWithVar "a" out
+
+      has :: ID_Var -> Type -> Bool
+      has "a" TAny            = False
+      has "a" TUnit           = False
+      has "a" (TVar x)        = ("a" == x)
+      has "a" (TData _ ofs)   = or $ map (has "a") ofs
+      has "a" (TTuple tps)    = any (has "a") tps
+      has "a" (TFunc inp out) = has "a" inp || has "a" out
 
 -- pat1::B = id2(neq) e2::(B,B)
 mE globs _ dsigs xtp e@(ECall z1 e2@(EVar z2 id2) e3) = ECall z1 e2' e3 where
@@ -96,8 +125,8 @@ mE globs _ dsigs xtp e@(ECall z1 e2@(EVar z2 id2) e3) = ECall z1 e2' e3 where
   recs2 = getRecDatas globs tp2
 
   toSimpleF' suf = ECall z2 (EVar z2 $ id2++"'")
-                      (fromList $ map (EVar z2) $ map toID $ cs2') where
-                      toID ifc = "d" ++ ifc ++ suf -- dIEqa / dIEqBool
+                    (fromList $ map (EVar z2) $ map toID $ cs2') where
+                    toID ifc = "d" ++ ifc ++ suf -- dIEqa / dIEqBool
 
   toComplexF' suf = ECall z2 (EVar z2 $ id2++"'")
                       (fromList $ map f $ cs2') where
